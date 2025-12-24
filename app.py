0b644e419a4d90a9566ef701c0620e50b82d1da7
Chiara <chiara.relevat@outlook.fr>
2025-12-24 16:38:11 +0100

    with nabvbar and wip on exp page

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/experiences")
+def experiences():
+    return render_template("experiences.html", home=index)
